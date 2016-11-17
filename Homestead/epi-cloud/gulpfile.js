cd93dc597e3e8664c0df5971a379568990b43e08
roucou_c <clement.roucour@epitech.eu>
2016-11-17 18:56:46 +0100

    remove theme

--- Homestead/epi-cloud/gulpfile.js
+++ Homestead/epi-cloud/gulpfile.js
@@ -13,7 +13,19 @@ require('laravel-elixir-vue-2');
  |
  */
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+<link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+<link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/css/zabuto_calendar.css">
+    <link rel="stylesheet" type="text/css" href="assets/js/gritter/css/jquery.gritter.css" />
+    <link rel="stylesheet" type="text/css" href="assets/lineicons/style.css">
+
+    <!-- Custom styles for this template -->
+<link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">elixir(function(mix) {
+
+    mix.styles([
+        'normalize.css',
+        'main.css'
+    ]);
 });
