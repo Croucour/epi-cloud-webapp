5b372e4c37e720dde9d9af4258e84b10bd0d3a1d
hugose <hugo.sempere@epitech.eu>
2016-11-29 00:10:03 +0100

    gitignore

---
+++ Homestead/epi-cloud/config/urls.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HugoS-Pc
+ * Date: 28/11/2016
+ * Time: 23:29
+ */
+
+return [
+    'VM_SERVER' => 'https://epi-cloud-vm-service.herokuapp.com/api/v1'
+];
