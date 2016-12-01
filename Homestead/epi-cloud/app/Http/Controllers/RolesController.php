853e2636759919e795ef05096ad0418dc208a73e
roucou_c <clement.roucour@epitech.eu>
2016-12-01 21:23:14 +0100

    route pour ajax changement role

--- Homestead/epi-cloud/app/Http/Controllers/RolesController.php
+++ Homestead/epi-cloud/app/Http/Controllers/RolesController.php
@@ -6,7 +6,12 @@ use App\Models\Boxes_waiting;
 use App\Models\Role;
 use App\Models\User;
 use App\Models\Boxes;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Ixudra\Curl\Facades\Curl;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
 
 class RolesController extends Controller
 {
@@ -23,6 +28,16 @@ class RolesController extends Controller
         $this->middleware('auth');
     }
 
+    public function jwtToken() {
+        $user = Auth::user();
+
+        $customClaims = ['user_id' => $user->id];
+
+        $payload = JWTFactory::make($customClaims);
+
+        return JWTAuth::encode($payload);
+    }
+
     /**
      * Show the application dashboard.
      *
@@ -34,5 +49,25 @@ class RolesController extends Controller
         $roles = Role::pluck('name', 'id');
 
         return view("roles.index")->with("users", $users)->with("roles", $roles);
+    }
+
+    public function update($user_id, $role_id) {
+        try {
+            $user = User::findOrFail($user_id);
+            $role = Role::findOrFail($role_id);
+
+            Role::detachAllRoles($user_id);
+            $user->attachRole($role);
+
+            $response = Curl::to(getenv('URL_API_FTP'))
+                ->returnResponseObject()
+                ->withHeader("Authorization: Bearer ".$this->jwtToken())
+                ->put();
+
+        }
+        catch (ModelNotFoundException $e) {
+            return response()->json(['response' => 500]);
+        }
+        return response()->json(['response' => 200]);
     }
 }
--- Homestead/epi-cloud/app/Models/Role.php
+++ Homestead/epi-cloud/app/Models/Role.php
@@ -10,4 +10,10 @@ class Role extends EntrustRole
     protected $name;
     protected $display_name;
     protected $description;
+
+    public static function detachAllRoles($user_id)
+    {
+        DB::table('role_user')->where('user_id', $user_id)->delete();
+
+    }
 }
